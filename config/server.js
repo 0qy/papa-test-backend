959fc980e7db56c1f14a48aed90d0eb1a62df83b
0qy <linqianye333@gmail.com>
2021-09-09 10:26:47 +1200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd08303802d5545fce0c9035ba809f1b3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c877e6ff-c2ee-40da-9eb6-cdf37bdf89c1'
+};
